a35f57a0fa569fee183f67a6962fb1ba27bc05f3
HyeonJin Song <shwj1602@naver.com>
2023-05-05 21:23:42 +0900

    Feat: JPA 어노테이션 추가

--- src/main/java/com/example/within/WithInApplication.java
+++ src/main/java/com/example/within/WithInApplication.java
@@ -2,8 +2,10 @@ package com.example.within;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WithInApplication {
 
     public static void main(String[] args) {
