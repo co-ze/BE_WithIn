64954dbf9296058a9ca149e07c009f363f829d78
HyeonJin Song <shwj1602@naver.com>
2023-05-05 20:20:59 +0900

    initial commit

---
+++ src/main/java/com/example/within/WithInApplication.java
@@ -0,0 +1,13 @@
+package com.example.within;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithInApplication.class, args);
+    }
+
+}
