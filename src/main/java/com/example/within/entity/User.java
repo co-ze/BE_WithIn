6188f830f1d2444ce19e57260d4ae0be528ed588
HyeonJin Song <shwj1602@naver.com>
2023-05-05 23:57:59 +0900

    Feat: Board 테이블과 매핑

--- src/main/java/com/example/within/entity/User.java
+++ src/main/java/com/example/within/entity/User.java
@@ -1,9 +1,12 @@
 package com.example.within.entity;
 
 import com.example.within.dto.UserRequestDto;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -32,7 +35,12 @@ public class User {
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
 
-    public User(String email, String password, UserRoleEnum role) {
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Board> board;
+
+    public User(String username, String email, String password, UserRoleEnum role) {
+        this.username = username;
         this.email = email;
         this.password = password;
         this.role = role;
