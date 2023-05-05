cf5a2ae335bc00ceb576d42966546fc433cce2d7
DongKyu Lee <dldlehdrbrb@gmail.com>
2023-05-05 23:08:33 +0900

    UserPage 개인 정보 조회 구현

--- src/main/java/com/example/within/controller/UserController.java
+++ src/main/java/com/example/within/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/within/")
+@RequestMapping("/within")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
