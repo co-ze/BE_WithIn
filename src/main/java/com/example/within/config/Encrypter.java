df847896546d2723f50ed523c62c0c2dbb350e00
cozy <wonje1108@gmail.com>
2023-05-07 21:03:33 +0900

    jasypt 적용

---
+++ src/main/java/com/example/within/config/Encrypter.java
@@ -0,0 +1,9 @@
+//package com.example.within.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//
+//@Autowired
+//@Qualifier("jasyptStringEncryptor")
+//public class Encrypter encryptor {
+//}
