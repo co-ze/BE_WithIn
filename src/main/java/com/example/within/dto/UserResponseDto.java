7613e92dbd46b13c67b72b10830321bd6eaa27a6
HyeonJin Song <shwj1602@naver.com>
2023-05-08 15:57:10 +0900

    Refactor: userId 추가

--- src/main/java/com/example/within/dto/UserResponseDto.java
+++ src/main/java/com/example/within/dto/UserResponseDto.java
@@ -1,16 +1,17 @@
 package com.example.within.dto;
 
-import com.example.within.entity.User;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserResponseDto {
+    private Long userId;
     private String username;
     private String email;
 
-    public UserResponseDto(String username, String email) {
+    public UserResponseDto(Long userId, String username, String email) {
+        this.userId = userId;
         this.username = username;
         this.email = email;
     }
