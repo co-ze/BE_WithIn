236bab320a211e9ce33f784395e8975831c9adc5
HyeonJin Song <shwj1602@naver.com>
2023-05-08 15:58:55 +0900

    Feat: Valid 예외처리 추가

--- src/main/java/com/example/within/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/within/exception/GlobalExceptionHandler.java
@@ -1,11 +1,35 @@
 package com.example.within.exception;
 
+import com.example.within.dto.BasicResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    /**
+     * Valid 예외 처리
+     */
+    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
+    public ResponseEntity signValidException(MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
+
+        StringBuilder builder = new StringBuilder();
+
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append("[");
+            builder.append(fieldError.getField());
+            builder.append("] ");
+            builder.append(fieldError.getDefaultMessage());
+        }
+        return new ResponseEntity(BasicResponseDto.addBadRequest(builder.toString()), HttpStatus.BAD_REQUEST);
+    }
     @ExceptionHandler(ErrorException.class)
     public ResponseEntity<ErrorResponseDto> handleErrorException(ErrorException e) {
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage(), e.getExceptionEnum().getStatus());
