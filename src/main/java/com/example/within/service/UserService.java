51b09d08ca2fb93a0ad36318a6a0bcda534dd7f8
DongKyu Lee <dldlehdrbrb@gmail.com>
2023-05-08 15:52:08 +0900

    cors 기능 추가 및 사진 크기 조정 코드 추가

--- src/main/java/com/example/within/service/UserService.java
+++ src/main/java/com/example/within/service/UserService.java
@@ -6,12 +6,12 @@ import com.example.within.entity.UserRoleEnum;
 import com.example.within.exception.ErrorException;
 import com.example.within.exception.ErrorResponseDto;
 import com.example.within.exception.ExceptionEnum;
-import com.example.within.repository.EmotionRepository;
 import com.example.within.repository.UserRepository;
 import com.example.within.util.FileUploadUtil;
 import com.example.within.util.JwtUtil;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.imgscalr.Scalr;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,6 +20,14 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -118,6 +126,15 @@ public class UserService {
         //User 클래스의 username 속석을 userPageRequestDto의 username값으로 설정
         userUpdate.setUsername(userPageRequestDto.getUsername());
 
+        // Resize and compress the image file
+        BufferedImage bufferedImage = ImageIO.read(imageFile.getInputStream());
+        BufferedImage resizedImage = Scalr.resize(bufferedImage, 300);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(resizedImage, "jpg", baos);
+        baos.flush();
+        byte[] compressedBytes = compressBytes(baos.toByteArray(), 0.7f, 3); // compress the image to have a file size of up to 3MB with 70% compression quality
+        baos.close();
+
         // Save the image file to disk
         String fileName = StringUtils.cleanPath(imageFile.getOriginalFilename());
         String fileExtension = StringUtils.getFilenameExtension(fileName); // StringUtils 클래스를 다시 사용하여서 정리된 파일 이름에서 파일 확장자 추출
@@ -133,5 +150,26 @@ public class UserService {
         // Return the updated user object
         return ResponseEntity.ok(userUpdate);
         // StringUtils 클래스를 사용하여 업로드 된 파일의 파일 이름을 정리 및 불필요한 문자나 피알 경로 정보 제거
+    }
+    private byte[] compressBytes(byte[] bytes, float quality, int maxSizeMB) throws IOException {
+        float compressionRatio = 1.0f;
+        int maxSizeBytes = maxSizeMB * 1024 * 1024;
+        while (bytes.length * compressionRatio > maxSizeBytes) {
+            compressionRatio -= 0.05f;
+        }
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(bytes));
+        ImageOutputStream ios = ImageIO.createImageOutputStream(baos);
+        ImageWriter writer = ImageIO.getImageWritersByFormatName("jpg").next();
+        ImageWriteParam writeParam = writer.getDefaultWriteParam();
+        writeParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        writeParam.setCompressionQuality(quality * compressionRatio);
+        writer.setOutput(ios);
+        writer.write(null, new IIOImage(bufferedImage, null, null), writeParam);
+        writer.dispose();
+        ios.flush();
+        byte[] compressedBytes = baos.toByteArray();
+        baos.close();
+        return compressedBytes;
     }
 }
