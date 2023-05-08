5fcb24fef7431c98c2645df0eb013988d51d63bc
HyeonJin Song <shwj1602@naver.com>
2023-05-08 15:59:21 +0900

    Feat: 메서드 추가

--- src/main/java/com/example/within/dto/BasicResponseDto.java
+++ src/main/java/com/example/within/dto/BasicResponseDto.java
@@ -1,13 +1,21 @@
 package com.example.within.dto;
 
+import com.example.within.entity.StatusCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor(staticName = "add")
 public class BasicResponseDto {
     private int statusCode;
     private String message;
+
+    public static BasicResponseDto addSuccess(int statusCode, String message){
+        return BasicResponseDto.add(statusCode, message);
+    }
+    public static BasicResponseDto addBadRequest(String message){
+        return BasicResponseDto.add(StatusCode.BAD_REQUEST.getStatusCode(), message);
+    }
 }
