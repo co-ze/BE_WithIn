71a35445e8381bdff270e707a3f921469b3c7a56
HyeonJin Song <shwj1602@naver.com>
2023-05-10 20:50:34 +0900

    Refactor: UserResponseDto img 값 추가

--- src/main/java/com/example/within/dto/UserResponseDto.java
+++ src/main/java/com/example/within/dto/UserResponseDto.java
@@ -9,10 +9,12 @@ public class UserResponseDto {
     private Long userId;
     private String username;
     private String email;
+    private String img;
 
-    public UserResponseDto(Long userId, String username, String email) {
+    public UserResponseDto(Long userId, String username, String email, String img) {
         this.userId = userId;
         this.username = username;
         this.email = email;
+        this.img = img;
     }
 }
